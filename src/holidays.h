fe969679ebfa9764c9e7b8d272f6ac2ab5796557
André Wählisch <AndreWaehlisch@users.noreply.github.com>
2024-06-30 00:23:12 +0200

    add 2025 and 2026 holidays

--- src/holidays.h
+++ src/holidays.h
@@ -16,6 +16,30 @@ QList<QDate> dateList = {
     QDate(2024, 10, 3), // Tag der dt. Einheit
     QDate(2024, 12, 25), // 1. Weihnachtstag
     QDate(2024, 12, 26), // 2. Weihnachtstag
+
+    // 2025
+    QDate(2025, 1, 1), // Neujahr
+    QDate(2025, 3, 8), // Frauentag
+    QDate(2025, 4, 18), // Karfreitag
+    QDate(2025, 4, 21), // Ostermontag
+    QDate(2025, 5, 1), // Tag der Arbeit
+    QDate(2025, 5, 29), // Christi Himmelfahrt
+    QDate(2025, 6, 9), // Pfingstmontag
+    QDate(2025, 10, 3), // Tag der dt. Einheit
+    QDate(2025, 12, 25), // 1. Weinachtstag
+    QDate(2025, 12, 26), // 2. Weinachtstag
+
+    // 2026
+    QDate(2026, 1, 1), // Neujahr
+    QDate(2026, 3, 8), // Frauentag
+    QDate(2026, 4, 3), // Karfreitag
+    QDate(2026, 4, 6), // Ostermontag
+    QDate(2026, 5, 1), // Tag der Arbeit
+    QDate(2026, 5, 14), // Christi Himmelfahrt
+    QDate(2026, 5, 25), // Pfingstmontag
+    QDate(2026, 10, 3), // Tag der dt. Einheit
+    QDate(2026, 12, 25), // 1. Weinachtstag
+    QDate(2026, 12, 26), // 2. Weinachtstag
 };
 
 #endif // HOLIDAYS_H
